c568a50c2920ec52d6ebdfd5d3c6e529259d7396
corydu <corydu@amazon.com>
2023-10-31 20:38:20 +0000

    Update test.sh for testing all examples

--- examples/test.sh
+++ examples/test.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-DIRS="01_getpid 02_libtiff 03_ffmpeg_custom_mutator 04_syscall_fuzzer"
+DIRS="01_getpid 02_libtiff 03_ffmpeg_custom_mutator 04_syscall_fuzzer 05_redqueen 06_custom_feedback"
 
 # Make and test all included examples
 for dir in $DIRS; do 
-  echo $dir
-  pushd $dir
-    ./reset.sh
-    ./make_example.sh
-  popd
-done
+  echo Testing $dir
+  pushd $dir >/dev/null
+    if ! [ -d ./snapshot ]; then 
+      ./make_example.sh
+    else 
+      pushd ./snapshot >/dev/null
+      ./reset.sh
+      popd >/dev/null
+    fi
 
-for dir in $DIRS; do
-  pushd $dir
     ./test.sh
-  popd
+  popd >/dev/null
 done
