02009d959f3ca5e36c0c7a7c7feb6de1b4542c1f
Michael Rodler <mrodler@amazon.de>
2023-12-07 17:48:49 +0000

    Updated example 01 `test.sh` with some more niceties. + added a Makefile for easier experimentation.

--- examples/01_getpid/test.sh
+++ examples/01_getpid/test.sh
@@ -1,77 +1,123 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+if [[ -z "$FUZZ_CORES" ]]; then
+    FUZZ_CORES="/2"
+fi
+
+if [[ -z "$FUZZ_TIMEOUT" ]]; then 
+    FUZZ_TIMEOUT="1m"
+fi
+
+EX="$(basename $PWD)"
+
+COLOR_CLEAR='\e[0m'
+COLOR_RED='\e[0;31m'
+COLOR_GREEN='\e[0;32m'
+
+function err {
+    echo -e "${COLOR_RED}ERROR: $EX - $* $COLOR_CLEAR"
+    exit 1
+}
+
+function log_success {
+    echo -e "${COLOR_GREEN}SUCCESS: $EX - $* $COLOR_CLEAR"
+}
+
+if ! test -d snapshot; then
+    err "require snapshot"
+fi
 
 # Reset the snapshot from a previous run
 pushd snapshot > /dev/null
-# ./reset.sh
+./reset.sh
 popd > /dev/null
 
 # Rebuild the fuzzer
 echo "Building Example 01"
-cargo build -r >/dev/null 2>/dev/null
+cargo build -r >/dev/null 2>/dev/null || err "build failed"
 
 # Seed the input with an easier input
 echo "Begin fuzzing!"
 mkdir -p snapshot/input
 echo -n fuzzmetosolvem11 > snapshot/input/test
-cargo run -r -- fuzz -c 4 --ascii-stats --stop-after-first-crash --stop-after-time 1m >/dev/null 2>/dev/null &
+cargo run -r -- fuzz -c "$FUZZ_CORES" --ascii-stats --stop-after-first-crash --stop-after-time "$FUZZ_TIMEOUT" >/dev/null 2>/dev/null
 
-# If we find a crash early, kill the fuzzer
-for i in $(seq 0 60); do
-  ls snapshot/crashes/SIGSEGV* >/dev/null 2>/dev/null
-  STATUS=$?
-  if [ "$STATUS" -eq 0 ]; then
-    kill -9 $PID 2>/dev/null >/dev/null
-    ps -ef | rg Example01 | tr -s ' ' | cut -d' ' -f2 | xargs kill -9 2>/dev/null >/dev/null
-    break
-  fi
-  sleep 1
-done
-
-# Kill the example 01 fuzzers
-ps -ef | rg Example01 | tr -s ' ' | cut -d' ' -f2 | xargs kill -9 2>/dev/null >/dev/null
+# # If we find a crash early, kill the fuzzer
+# for i in $(seq 0 60); do
+#   ls snapshot/crashes/SIGSEGV* >/dev/null 2>/dev/null
+#   STATUS=$?
+#   if [ "$STATUS" -eq 0 ]; then
+#     kill -9 $PID 2>/dev/null >/dev/null
+#     ps -ef | rg Example01 | tr -s ' ' | cut -d' ' -f2 | xargs kill -9 2>/dev/null >/dev/null
+#     break
+#   fi
+#   sleep 1
+# done
+#
+# # Kill the example 01 fuzzers
+# ps -ef | rg Example01 | tr -s ' ' | cut -d' ' -f2 | xargs kill -9 2>/dev/null >/dev/null
 
 # Check if the fuzzer found a crash
 ls snapshot/crashes/SIGSEGV* >/dev/null
 STATUS=$?
 if [ "$STATUS" -gt 0 ]; then
-  echo "Example 1 did not find crash"
-  exit 1
+    err "did not find crash"
 fi
 
-echo -e "\e[32mExample 01 fuzz SUCCESS!\e[0m"
 
-CRASH_FILE=$(find ./snapshot/crashes/SIGSEGV* -type f | head -n 1) 
+log_success "fuzz"
+
+CRASH_FILE="$(find ./snapshot/crashes/SIGSEGV* -type f | head -n 1)"
+CORPUS_FILE="$(find ./snapshot/current_corpus/ -type f | tail -n 1)"
 
 ### Test trace ###
 
 ## trace -- basic
-cargo run -r -- trace $CRASH_FILE 2>/dev/null >/dev/null
+cargo run -r -- trace "$CRASH_FILE" 2>/dev/null >/dev/null || err "running trace failed"
 
-grep unhandled_signal snapshot/traces/* >/dev/null
-STATUS=$?
-if [ "$STATUS" -gt 0 ]; then
-  echo "Example 1 failed trace test -- basic"
-  exit 1
-fi
+grep unhandled_signal snapshot/traces/* >/dev/null || err "trace test basic - no unhandled_signal"
 
 ## trace -- no-single-step
 cargo run -r -- trace $CRASH_FILE --no-single-step 2>/dev/null >/dev/null
-grep INSTR snapshot/traces/*no_single_step >/dev/null
-STATUS=$?
-if [ "$STATUS" -gt 0 ]; then
-  echo "Example 1 failed trace test -- --no-single-step"
-  exit 1
-fi
+grep INSTR snapshot/traces/*no_single_step >/dev/null || err "trace test no-single-step - no unhandled_signal"
 
-echo -e "\e[32mExample 01 trace SUCCESS!\e[0m"
+log_success "trace"
 
 ### Test coverage ###
 
-## coverage
-cargo run -r -- coverage $CRASH_FILE 2>/dev/null >/dev/null
-ls coverages >/dev/null
-if [ "$STATUS" -gt 0 ]; then
-  echo "Example 1 failed coverage test"
-  exit 1
+if ! [[ -e ./snapshot/coverage.lcov ]]; then
+    ls -al ./snapshot/coverage*
+    err "no lcov output after fuzzing"
 fi
-echo -e "\e[32mExample 01 coverage SUCCESS!\e[0m"
+lcov_counts="$(grep -c ",1" ./snapshot/coverage.lcov)"
+if [[ "$lcov_counts" -lt 5 ]]; then
+    err "lcov coverage output seems wrong with $lcov_counts hit lines"
+fi
+
+## coverage
+cargo run -r -- coverage $CRASH_FILE 2>/dev/null >/dev/null || err "running coverage subcommand failed"
+ls snapshot/coverage_per_input/ >/dev/null
+STATUS=$?
+if [ "$STATUS" -gt 0 ]; then
+    err "coverage command did not yield per-input coverage"
+fi
+log_success "coverage"
+
+### Test minimize ###
+cargo run -r -- minimize "$CRASH_FILE" 2>/dev/null >/dev/null || err "running minimize subcommand failed"
+# if [ "$STATUS" -gt 0 ]; then
+#   echo "Example 1 failed minimize test"
+#   exit 1
+# fi
+cargo run -r -- minimize "$CORPUS_FILE" 2>/dev/null >/dev/null || err "running minimize subcommand failed"
+
+log_success "minimize"
+
+### Test corpus-min ###
+PRIOR="$(ls -l ./snapshot/current_corpus | wc -l)"
+cargo run -r -- corpus-min 2>/dev/null >/dev/null || err "running corpus-min subcommand failed"
+NEW="$(ls -l ./snapshot/current_corpus | wc -l)"
+if [ "$NEW" -ge "$PRIOR" ]; then
+    err "corpus-min did not reduce corpus size"
+fi
+log_success "corpus-min"
